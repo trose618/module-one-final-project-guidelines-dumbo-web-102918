df7c848ca66e331d837d09b8732de37b0d290ee1
trose618 <trose618@gmail.com>
2018-11-13 11:47:21 -0500

    successfully tested user/match relations

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,18 @@
 require_relative '../config/environment'
+require 'pry'
 
+users = [" ", " "]
 
+users.each do |user|
+  puts "Enter your name."
+  nam = gets
+  puts "Enter your strength"
+  str = gets
+  puts "Enter your weakness"
+  wkn = gets
+  User.create(name: nam, strength: str, weakness: wkn)
+end
+
+binding.pry
 
 puts "HELLO WORLD"
